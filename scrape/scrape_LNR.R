b42150560e1ffcabf85ee0fe095cfb0a05aa9262
theo <theo@localhost.localdomain>
2026-05-07 14:15:29 +0200

    Reducing two scraping data and stats functions into scrapeLNR and allowing to choose specific rounds in specific seasons.

--- scrape/scrape_LNR.R
+++ scrape/scrape_LNR.R
@@ -6,190 +6,153 @@ source("scrape_annex.R")
 # BEGINNING #-# BEGINNING #-# BEGINNING #-# BEGINNING #-# BEGINNING #-# BEGINNING #-# BEGINNING #-# BEGINNING #
 ##-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-#-##
 
-findMatchLNR <- function(union){
+#Find for every seasons, every rounds. Number rounds, final, etc. can change depending on the season.
+findMatchLNR <- function(union,verbose=TRUE){
   if(!is.element(union, c("prod2","top14"))) stop("Union not in the list.")
   url <- paste0("https://",union,".lnr.fr/calendrier-et-resultats/")
   b <- ChromoteSession$new()
   navigate_safe(b,url,"input--select",retries = 2)
+  Sys.sleep(0.5)
   tmp <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.input--select')).map(el => el.innerText))")$result$value)
-  return(list("season"=strsplit(tolower(tmp[1]),"\n")[[1]][-1],"round"=strsplit(tolower(tmp[2]),"\n")[[1]][-1]))
+  season <- strsplit(tolower(tmp[1]),"\n")[[1]][-1]
+  res <- list()
+  for(s in season){
+    if(verbose) print(s)
+    js <- sprintf("
+  new Promise(resolve => {
+    const select = document.querySelector('#Saison');
+    if(!select){ resolve('no select'); return; }
+    const getText = () => select.options[select.selectedIndex]?.text.trim();
+    const targetText = '%s';
+    const opts = Array.from(select.options);
+    const idx = opts.findIndex(o => o.text.trim() === targetText);
+    if(idx >= 0){
+      select.selectedIndex = idx;
+      select.dispatchEvent(new Event('input',{bubbles:true}));
+      select.dispatchEvent(new Event('change',{bubbles:true}));
+    }
+
+    const check = () => {
+      if(getText() === targetText){
+        resolve('updated');
+      } else {
+        setTimeout(check, 100);
+      }
+    };
+
+    check();
+  })
+", s)
+    js <- gsub("[\n\r]", " ", js)
+    b$Runtime$evaluate(expression = js, awaitPromise = TRUE)
+    res[[s]] <- strsplit(tolower(jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.input--select')).map(el => el.innerText))")$result$value)[2]),"\n")[[1]][-1]
+  }
+  b$close()
+  return(res)
 }
 
 # union <- c("top14") #c("prod2")  c("top14")
-scrapeLNR<- function(union,addMat=NULL,whichSeasons=NULL,whichRounds=NULL){
+scrapeLNR <- function(union,addMat=NULL,whichMatches=NULL,fetchdata=TRUE,fetchstats=TRUE,verbose=TRUE){
+  ### Checking arguments ### 
+  if(!fetchdata & !fetchstats) stop("Either fetchstats or fetchdata must be TRUE.")
   if(!is.element(union, c("prod2","top14"))) stop("Union not in the list.")
-  if(!is.null(addMat)) if(any(colnames(addMat)!=c("minute","type","home","away","where","score","round","player","date"))) stop("Cannot add to the given matrix. Wrong colnames.")
-  tmp <- findMatchLNR(union)
-  listSeason <- tmp$season
-  listRound <- tmp$round
-  if(is.null(whichRounds) || any(!is.element(whichRounds,listRound)) ){ stop("Wrong seasons. Check findMatchLNR().")
-  } else { listSeason <- whichSeasons }
-  if(is.null(whichSeasons) || any(!is.element(whichSeasons,listSeason)) ){ stop("Wrong rounds. Check findMatchLNR().")
-  } else { listRound <- whichRounds }
-  
-  id <- array(dim=c(10,length(listRound),length(listSeason)))  # 1: number of matches, 2: number of rounds, 3: number of seasons 
-  dimnames(id)[[3]] <- listSeason
-  dimnames(id)[[2]] <- listRound
-  for(s in listSeason){
-    url <- paste0("https://",union,".lnr.fr/calendrier-et-resultats/",s,"/")
-    for (j in listRound) {
-      download.file(paste0(url,j,collapse=""),"lnr.html")
-      lines <- readLines("lnr.html")
-      pat <- paste0("https://",union,".lnr.fr/feuille-de-match/",s,"/",j,"/")
-      link <- findlinerow(lines,pat)
-      if(is.null(link)) next
-      else{
-        link <- link[rep(c(FALSE,TRUE),nrow(link)/2),]
-        for(i in 1:nrow(link)) id[i,j,s] <- strsplit(substr(lines[link[i,1]],link[i,2],link[i,2]+10000),"'")[[1]]
-      }
+  if(!is.null(addMat)) if(!is.list(whichMatches) | any(colnames(addMat$data)!=c("minute","type","home","away","where","score","round","player","date")) | any(colnames(addMat$stats)!=c("type","home","away","statsHome","statsAway","round","date"))) stop("Cannot add to the given matrices. Wrong colnames or not a list.")
+  ### Retrieving possible seasons and rounds ### 
+  tmp <- findMatchLNR(union,FALSE)
+  if(length(tmp)==0) stop("Probably no internet connection.")
+  listSeason <- names(tmp)
+  listRound <- tmp
+  ### Check if the wanting seasons and rounds are effectively existing ### 
+  if(any(!is.element(names(whichMatches),listSeason)) ){ 
+    stop("Wrong seasons. Check findMatchLNR().")
+  } else if(!is.null(whichMatches)) { 
+    listSeason <- names(whichMatches) 
+    listRound <- listRound[listSeason]
+    for(s in listSeason){
+      if(any(!is.element(whichMatches[[s]],listRound[[s]])) ){ stop("Wrong rounds. Check findMatchLNR().")
+      } else if(!is.null(whichMatches)){ listRound[[s]] <- whichMatches[[s]] }
     }
   }
-  # id <- id[!apply(id,1,function(vec) all(is.na(vec))),!apply(id,2,function(vec) all(is.na(vec))),!apply(id,3,function(vec) all(is.na(vec)))]
-  
+  ### Scraping data and stats ### 
   data <- data.frame("minute"=NA,"type"=NA,"home"=NA,"away"=NA,"where"=NA,"score"=NA,"round"=NA,"player"=NA,"date"=NA)
+  stats <- data.frame("type"=NA,"home"=NA,"away"=NA,"statsHome"=NA,"statsAway"=NA,"round"=NA,"date"=NA)
   for(s in listSeason){
-    for(jou in listRound){
-      for(idm in id[,jou,s][!is.na(id[,jou,s])]){
-        tmp <- strsplit(idm,"-")[[1]]
-        taway <- strsplit(idm,"-")[[1]][length(tmp)]
-        thome <- strsplit(idm,"-")[[1]][2]
-        round <- substr(jou,2,100)
-        print(paste(s,jou,thome,taway))
-        url <- paste0("https://",union,".lnr.fr/feuille-de-match/",s,"/",jou,"/",idm)
-        
-        b <- ChromoteSession$new()
-        navigate_safe(b,url,"title",retries = 2)
-        tmp <- strsplit(jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.title.title--large.title--textured.title--centered')).map(el => el.innerText))")$result$value)," - ")     
-        score <- list("home"=tmp[[1]][1],"away"=tmp[[1]][2])
-        navigate_safe(b,url,"match-header__season-day",retries = 2)
-        tmp <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.match-header__season-day')).map(el => el.innerText))")$result$value)
-        date <- strsplit(tmp," ")[[1]][6]
-        
-        flag <- navigate_safe(b,url,"vertical-timeline-item__time-label",retries = 2)
+    for(j in listRound[[s]]){
+      b <- ChromoteSession$new()
+      flag <- navigate_safe(b,paste0("https://",union,".lnr.fr/calendrier-et-resultats/",s,"/",j,collapse=""),"match-line__score",retries = 2,timeout = 1000)
+      if(!flag) next
+      id <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.match-line__score')).map(el => el.href))")$result$value)
+      for(idm in id){
+        ### Finding home team, visitor team, round, date ###
+        flag <- navigate_safe(b,idm,"title",retries = 2)
         if(!flag) next
-        
-        for(where in c("home","away")){
-          action <- jsonlite::fromJSON(b$Runtime$evaluate(paste0("
+        tmp <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.title.title--large.title--textured.title--centered')).map(el => el.innerText))")$result$value)
+        if(length(tmp) > 0) score <- list("home"=strsplit(tmp," - ")[[1]][1],"away"=strsplit(tmp," - ")[[1]][2]) 
+        tmp <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.match-header__season-day')).map(el => el.innerText))")$result$value)
+        if(length(tmp) > 0){
+          round <- tolower(strsplit(tmp," ")[[1]][4])
+          date <- strsplit(tmp," ")[[1]][6]
+        }
+        tmp <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.match-header-club__title')).map(el => el.innerText))")$result$value)
+        thome <- tolower(tmp[1])
+        taway <- tolower(tmp[2])
+        if(verbose) print(paste(s,j,thome,taway))
+        ### Fetch data ###
+        if(fetchdata){
+          for(where in c("home","away")){
+            action <- jsonlite::fromJSON(b$Runtime$evaluate(paste0("
               JSON.stringify(
                 Array.from(document.querySelectorAll('.vertical-timeline-item--",where," .vertical-timeline-item__wrapper')).map(el => el.innerText)
               )"))$result$value)
-          actime <- jsonlite::fromJSON(b$Runtime$evaluate(paste0("
+            actime <- jsonlite::fromJSON(b$Runtime$evaluate(paste0("
               JSON.stringify(
                 Array.from(document.querySelectorAll('.vertical-timeline-item--",where," .vertical-timeline-item__time-label')).map(el => el.innerText)
               )"))$result$value)
-          for(i in 1:length(action)){
-            tmp <- strsplit(action[i],"\n")[[1]]
-            if(length(tmp)==3) data <- rbind(data,c(actime[i],tmp[2],thome,taway,where,score[[where]],round,tolower(tmp[3]),date))
-            else if(length(tmp)==4){
-              tmpbis <- strsplit(tmp[4]," ")[[1]]
-              data <- rbind(data,c(actime[i],"conversion",thome,taway,where,score[[where]],round,tolower(paste(tmpbis[3:length(tmp)],collapse=" ")),date))
+            if(length(action)>0){
+              for(i in 1:length(action)){
+                tmp <- strsplit(action[i],"\n")[[1]]
+                if(length(tmp)==3) data <- rbind(data,c(actime[i],tmp[2],thome,taway,where,score[[where]],round,tolower(tmp[3]),date))
+                else if(length(tmp)==4){
+                  tmpbis <- strsplit(tmp[4]," ")[[1]]
+                  data <- rbind(data,c(actime[i],"conversion",thome,taway,where,score[[where]],round,tolower(paste(tmpbis[3:length(tmp)],collapse=" ")),date))
+                }
+              }
             }
-          } 
+          }
         }
-        b$close()
+        ### Fetch stats ###
+        if(fetchstats){
+          flag <- navigate_safe(b,paste0(idm,"/statistiques-du-match"),"stats-bar",retries = 2,timeout = 1000)
+          if(!flag) next
+          for(where in c("home","away")){
+            action <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.stats-bar')).map(el => el.innerText))")$result$value)
+            if(length(action)>0){
+              for(i in 1:length(action)){
+                tmp <- strsplit(action[i],"\n")[[1]]
+                stats <- rbind(stats,c(tmp[1],thome,taway,tmp[2],tmp[3],round,date))
+              } 
+            }
+          }
+        }
+        
       }
+      b$close()
     }
   }
   data <- data[-1,]
-  if(union=="top14"){
-    tradteamgene <- c("bayonne","bordeaux-bègles","castres","clermont","la rochelle","lyon","montauban","montpellier","pau","perpignan","racing 92","stade français","stade toulousain","toulon")
-    tradteamlnr <- c("bayonne","bordeaux","castres","clermont","la","lyon","montauban","montpellier","pau","perpignan","racing","paris","toulouse","toulon")
-  }
-  if(union=="prod2"){
-    tradteamgene <- c("agen","angoulême","aurillac","béziers","biarritz","brive","carcassonne","colomiers","dax","grenoble","mont de marsan","nevers","oyonnax","provence rugby","valence romans","vannes")
-    tradteamlnr <- c("agen","angouleme","aurillac","beziers","biarritz","brive","carcassonne","colomiers","dax","grenoble","mont","nevers","oyonnax","provence","valence","vannes")
-  }
-  for(j in 1:length(tradteamlnr)) for(where in c("home","away")) data[where][data[where]==tradteamlnr[j]] <- tradteamgene[j]
-  
+  stats <- stats[-1,]
+  ### Translating from French to English ###
   tradtypegene <- c("try","penalty try","penalty","drop","conversion","yellow","red","orange")
   tradtypelnr <- c("Essai","Essai de pénalité","Pénalité","Drop","conversion","Carton Jaune","Carton Rouge","Carton Orange")
   for(j1 in 1:length(tradtypegene)) data$type[data$type==tradtypelnr[j1]] <- tradtypegene[j1]
-  
-  if(is.null(addMat)){ return(data)   
+  ### Concatenating input and output ###
+  if(is.null(addMat)){ return(list("data"=data,"stats"=stats))   
   } else {
-    addMat <- rbind(addMat,data)
-    return(addMat[!duplicated(addMat),])
-  }
-}
-
-# union <- c("top14") #c("prod2")  c("top14")
-scrapeLNRstats<- function(union,addMat=NULL,whichSeasons=NULL,whichRounds=NULL){
-  if(!is.element(union, c("prod2","top14"))) stop("Union not in the list.")
-  if(!is.null(addMat)) if(any(colnames(addMat)!=c("type","home","away","statsHome","statsAway","round","date"))) stop("Cannot add to the given matrix. Wrong colnames.")
-  tmp <- findMatchLNR(union)
-  listSeason <- tmp$season
-  listRound <- tmp$round
-  if(is.null(whichRounds) || any(!is.element(whichRounds,listRound)) ){ stop("Wrong seasons. Check findMatchLNR().")
-  } else { listSeason <- whichSeasons }
-  if(is.null(whichSeasons) || any(!is.element(whichSeasons,listSeason)) ){ stop("Wrong rounds. Check findMatchLNR().")
-  } else { listRound <- whichRounds }
-  
-  id <- array(dim=c(10,length(listRound),length(listSeason)))  # 1: number of matches, 2: number of rounds, 3: number of seasons 
-  dimnames(id)[[3]] <- listSeason
-  dimnames(id)[[2]] <- listRound
-  for(s in listSeason){
-    url <- paste0("https://",union,".lnr.fr/calendrier-et-resultats/",s,"/")
-    for (j in listRound) {
-      download.file(paste0(url,j,collapse=""),"lnr.html")
-      lines <- readLines("lnr.html")
-      pat <- paste0("https://",union,".lnr.fr/feuille-de-match/",s,"/",j,"/")
-      link <- findlinerow(lines,pat)
-      if(is.null(link)) next
-      else{
-        link <- link[rep(c(FALSE,TRUE),nrow(link)/2),]
-        for(i in 1:nrow(link)) id[i,j,s] <- strsplit(substr(lines[link[i,1]],link[i,2],link[i,2]+10000),"'")[[1]]
-      }
-    }
-  }
-  # id <- id[!apply(id,1,function(vec) all(is.na(vec))),!apply(id,2,function(vec) all(is.na(vec))),!apply(id,3,function(vec) all(is.na(vec)))]
-  
-  stats <- data.frame("type"=NA,"home"=NA,"away"=NA,"statsHome"=NA,"statsAway"=NA,"round"=NA,"date"=NA)
-  for(s in listSeason){
-    for(jou in listRound){
-      for(idm in id[,jou,s][!is.na(id[,jou,s])]){
-        tmp <- strsplit(idm,"-")[[1]]
-        taway <- strsplit(idm,"-")[[1]][length(tmp)]
-        thome <- strsplit(idm,"-")[[1]][2]
-        round <- substr(jou,2,100)
-        print(paste(s,jou,thome,taway))
-        
-        url <- paste0("https://",union,".lnr.fr/feuille-de-match/",s,"/",jou,"/",idm,"/statistiques-du-match")
-        b <- ChromoteSession$new()
-        navigate_safe(b,url,"match-header__season-day",retries = 2)
-        tmp <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.match-header__season-day')).map(el => el.innerText))")$result$value)
-        date <- strsplit(tmp," ")[[1]][6]
-        
-        flag <- navigate_safe(b,url,"stats-bar",retries = 2)
-        if(!flag) next
-        
-        for(where in c("home","away")){
-          try({
-            action <- jsonlite::fromJSON(b$Runtime$evaluate("JSON.stringify(Array.from(document.querySelectorAll('.stats-bar')).map(el => el.innerText))")$result$value)
-            for(i in 1:length(action)){
-              tmp <- strsplit(action[i],"\n")[[1]]
-              stats <- rbind(stats,c(tmp[1],thome,taway,tmp[2],tmp[3],round,date))
-            } 
-          },silent=TRUE)
-        }
-        b$close()
-      }
-    }
-  }
-  stats <- stats[-1,]
-  if(union=="top14"){
-    tradteamgene <- c("bayonne","bordeaux-bègles","castres","clermont","la rochelle","lyon","montauban","montpellier","pau","perpignan","racing 92","stade français","stade toulousain","toulon")
-    tradteamlnr <- c("bayonne","bordeaux","castres","clermont","la","lyon","montauban","montpellier","pau","perpignan","racing","paris","toulouse","toulon")
-  }
-  if(union=="prod2"){
-    tradteamgene <- c("agen","angoulême","aurillac","béziers","biarritz","brive","carcassonne","colomiers","dax","grenoble","mont de marsan","nevers","oyonnax","provence rugby","valence romans","vannes")
-    tradteamlnr <- c("agen","angouleme","aurillac","beziers","biarritz","brive","carcassonne","colomiers","dax","grenoble","mont","nevers","oyonnax","provence","valence","vannes")
-  }
-  for(j in 1:length(tradteamlnr)) for(where in c("home","away")) stats[where][stats[where]==tradteamlnr[j]] <- tradteamgene[j]
-  
-  if(is.null(addMat)){ return(stats)   
-  } else {
-    addMat <- rbind(addMat,stats)
-    return(addMat[!duplicated(addMat),])
+    addMat$data <- rbind(addMat$data,data)
+    addMat$data[!duplicated(addMat$data),]
+    addMat$stats <- rbind(addMat$stats,stats)
+    addMat$stats[!duplicated(addMat$stats),]
+    return(addMat)
   }
 }
 
