d2f1e732eb387b7e51d140502fbc4f99181c84d3
theo <theo@localhost.localdomain>
2026-05-07 15:07:02 +0200

    correcting a pat into a pat, an inverting type and label in the order of the arguments.

--- scrape/scrape_annex.R
+++ scrape/scrape_annex.R
@@ -2,7 +2,7 @@ library(xml2)
 
 #find in an html text the line (in lines) and the row (in the corresponding string) in which appear the last character of pat
 findlinerow <- function(lines,pat){
-  if(length(grep("pat",lines))==0) return(NULL)
+  if(length(grep(pat,lines))==0) return(NULL)
   else{
     res <- matrix(grep(pat,lines),ncol=1)
     res <- cbind(res, sapply(1:nrow(res), function(i) gregexpr(pat,lines[res[i,1]])[[1]][1] + nchar(pat)))
@@ -15,14 +15,14 @@ findlinerowextract <- function(lines,pat,to,plus=0){
   return( substr(lines[ind[1]],ind[2]+plus,ind[2]+plus+to) )  
 }
 #wrap repetitive elements used below. It uses values defined outside of the function scope, as authorized in R, so it must be used after the line 50
-findtypematch <- function(weblines,type,label,indlocal,thome,taway,scorehome,scoreaway,round,date,data){ 
-  indweb <- grep(type,weblines)
+findtypematch <- function(weblines,label,type,indlocal,thome,taway,scorehome,scoreaway,round,date,data){ 
+  indweb <- grep(label,weblines)
   if(length(indweb)>0){
     for(i in 1:length(indweb)){
       tmp <- c()
       tmp <- c(tmp, strsplit(findlinerowextract(weblines[indweb[i]],"span_idalgo_content_match_action_part_minute",5,2),'<')[[1]][1] )
-      if(is.element(indweb[i],indlocal)) tmp <- c(tmp,label,thome,taway,"home",scorehome)
-      else tmp <- c(tmp,label,thome,taway,"away",scoreaway)
+      if(is.element(indweb[i],indlocal)) tmp <- c(tmp,type,thome,taway,"home",scorehome)
+      else tmp <- c(tmp,type,thome,taway,"away",scoreaway)
       tmp <- c(tmp,round)   #round
       name <- strsplit(findlinerowextract(weblines[indweb[i]],"a_idalgo_content_match_action_part_player",1000000), ">")[[1]][2]
       tmp <- c(tmp, tolower(xml_text(read_html(paste0("<x>", substr(name,1,nchar(name)-3), "</x>")))),date)
